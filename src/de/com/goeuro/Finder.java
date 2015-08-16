c023ce82be189fed6badb61a4387376be7553749
ozge <gokayozge@gmail.com>
2015-08-16 15:54:46 +0300

    --

--- src/de/com/goeuro/Finder.java
+++ src/de/com/goeuro/Finder.java
@@ -1,47 +1,46 @@
 package de.com.goeuro;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.apache.commons.io.FileUtils;
 import org.json.CDL;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.json.JSONWriter;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 public class Finder {
 
+	private static final Logger LOGGER = Logger.getLogger(Finder.class.getName());
 	private static String city = "";
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException,JSONException{
 		Finder f = new Finder();
 		try {
 			city = args[0];
 			//city = "berlin";
 			JSONArray array = Finder.readJsonArrayFromUrl(city);
 			Finder.write(array);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
+		} catch (Exception e ) {
+			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
+		} 
 	}
 	public static JSONArray readJsonArrayFromUrl(String cityName) throws IOException, JSONException {
 		URL url = new URL("http://api.goeuro.com/api/v2/position/suggest/en/".concat(cityName));
+		JSONArray array = null;
 		try {
 			Reader bd =  new InputStreamReader(url.openStream(), StandardCharsets.UTF_8);
-			JSONArray array = new JSONArray(new JSONTokener(bd));
+			array = new JSONArray(new JSONTokener(bd));
 			return array;
-		} finally {
-
+		} catch(IOException e) {
+			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
+			throw new IOException(e);
 		}
 	}
 
@@ -66,9 +65,10 @@ public class Finder {
 			try {
 				FileUtils.writeStringToFile(file, csv);
 			} catch (IOException e) {
+				LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
 			}
 		} catch (JSONException e) {
-			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, e.getLocalizedMessage(), e);
 		}
 
 	}
